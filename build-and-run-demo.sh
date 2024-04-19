2b9c9a9a14c0fe3cb974b79aef8ce49f29a5a74c
Wojciech Przybylski <01168910@pw.edu.pl>
2024-04-19 00:57:45 +0000

    GL-3: Add g++ to ubuntu scripts

--- build-and-run-demo.sh
+++ build-and-run-demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install -y gcc cmake ninja-build git xorg-dev libgl1-mesa-dev libudev-dev libalut-dev libvorbis-dev libflac-dev
+apt install -y gcc g++ cmake ninja-build git xorg-dev libgl1-mesa-dev libudev-dev libalut-dev libvorbis-dev libflac-dev
 cmake --preset="main-release" . 
 cmake --build ./build/main-release
 ./build/main-release/bin/HoMMybeer3
--- scripts/install-requirements.sh
+++ scripts/install-requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt install -y gcc cmake ninja-build git xorg-dev libgl1-mesa-dev libudev-dev libalut-dev libvorbis-dev libflac-dev
+apt install -y gcc g++ cmake ninja-build git xorg-dev libgl1-mesa-dev libudev-dev libalut-dev libvorbis-dev libflac-dev
