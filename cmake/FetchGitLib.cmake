1d0ba3b0315566f823c699f535c300a519ae240c
Wojciech Przybylski <01168910@pw.edu.pl>
2024-05-26 15:48:50 +0200

    Add compiler warnings

--- cmake/FetchGitLib.cmake
+++ cmake/FetchGitLib.cmake
@@ -5,6 +5,7 @@ macro(fetch_git_lib NAME GIT_URL GIT_TAG)
         GIT_REPOSITORY ${GIT_URL}
         GIT_TAG ${GIT_TAG}
         USES_TERMINAL_DOWNLOAD ON
+        SYSTEM
     )
     FetchContent_MakeAvailable(${NAME})
     message(STATUS "Completed fetching git library ${NAME}")
